f8922ef6a7113311d1ff48d6055ce5be4e645461
kenedyLima <kenedytorreslima@gmail.com>
2022-08-09 09:48:41 -0300

    date sort

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -4,10 +4,12 @@ import { getDocs, onSnapshot } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 import {db} from "../firebase.config";
 import { useEffect } from "react";
-let docs = [];
-function ChatRoom({ sendMessageHandler, handleSignOut }) {
-  console.log("rendering chat");
-  console.log(docs);
+
+function ChatRoom({ sendMessageHandler, handleSignOut, docs, user }) {
+  const userName = user.displayName;
+  let setDocs;
+  [docs, setDocs] = useState(docs);
+  console.log(docs)
   return (
     <div className="chat">
       <div className="chat-header">
@@ -20,19 +22,44 @@ function ChatRoom({ sendMessageHandler, handleSignOut }) {
         <button onClick={handleSignOut}>Sign Out</button>
       </div>
       <div className="messages-container">
-   
+      {docs.length > 0 ? docs.map((doc, index) => {
+        const date = doc.data().date.toDate();
+  
+        return <div key={index}  className={userName === doc.data().name ? "message-wrapper user-message": "message-wrapper others-message"}><div className="author-time-wrapper"><span className="message-author">{doc.data().name}</span> <span className="message-time">{date.getHours()}:{date.getMinutes()}</span></div><p className="message">{doc.data().message}</p></div>
+
+      }) : "No messages yet!"}
       </div>
       <InputMessageField sendMessageHandler={sendMessageHandler} />
     </div>
   );
 }
 
-export function updateMessages(db) {
-  const unsub = onSnapshot(collection(db, "messages"), (messagesRef) => {
-    docs = messagesRef.docs;
-    console.log(docs)
-  })
+function dateComparator(array) {
+  const resultArray = [];
+  for(let i = 0; i < array.length; i++){
+    const currentElement = array[i];
+    for(let j = i; j < array.length; j++) {
+      let smallerElement;
+      let lastElementIndex;
+      if(lastElementIndex) {
+
+      }
+      if(array[i] <= array[j]){
+        resultArray[i] = array[i]
+        lastElementIndex = i;
+      } else {
+        resultArray[i] = array[j];
+        lastElementIndex = j;
+      }
+
+
+    }
+  }
+console.log("resultArray: ", resultArray);
 }
+
+dateComparator([5, 4, 3, 2, 1]);
+
 
 
 
